72031f31151a55b93c54104d8f9f70010caea8e5
rommsen <sarom@web.de>
2012-11-04 16:43:51 +0100

    widget-sort watches widgets

--- src/Coruja/PlesyndBundle/Resources/public/app/widget/widget-sort-directive.js
+++ src/Coruja/PlesyndBundle/Resources/public/app/widget/widget-sort-directive.js
@@ -4,27 +4,31 @@ Application.Directives.directive('widgetSort', ['widgetService',
     function (widgetService) {
         return  {
             'link' : function (scope, element, attrs, ctrl) {
-                //var scope = this;
-                // eventuell ein watch einbauen, um auf löschen etc von widgets reagieren zu können
+                function updateWidgets() {
+                    var widget,
+                        position = 1;
+                    element.find('.widget').each(function(index, element){
+                        widget = angular.element(element).scope().widget;
+                        if(scope.isWidgetVisible(widget)) {
+                            if(widget.position != position) {
+                                widget.position = position;
+                                widgetService.put(widget);
+                            }
+                            position += 1;
+                        }
+                    });
+                }
 
+                scope.$watch('widgets', function () {
+                    updateWidgets();
+                }, true);
 
                 element.sortable({
                     items : '.widget',
                     cursorAt: { top: 5, left: 5 },
                     tolerance: "pointer",
                     stop : function (event, ui) {
-                        var widget;
-                        var position = 1;
-                        $(".widget").each(function(index, element){
-                            widget = angular.element(element).scope().widget;
-                            if(scope.isWidgetVisible(widget)) {
-                                if(widget.position != position) {
-                                    widget.position = position;
-                                    widgetService.put(widget);
-                                }
-                                position += 1;
-                            }
-                        });
+                        updateWidgets();
                     }
 
                 });
