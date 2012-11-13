c4464f855cb418a06cae3b0ed0202ecba4913036
rommsen <sarom@web.de>
2012-11-13 12:55:02 +0100

    fixed minification error

--- src/Coruja/TodoBundle/Resources/public/app/todo/todo-focus-directive.js
+++ src/Coruja/TodoBundle/Resources/public/app/todo/todo-focus-directive.js
@@ -3,7 +3,7 @@
 /**
  * Directive that places focus on the element it is applied to when the expression it binds to evaluates to true.
  */
-Application.Directives.directive('todoFocus', [function( $timeout ) {
+Application.Directives.directive('todoFocus', ['$timeout', function( $timeout ) {
   return function( scope, elem, attrs ) {
     scope.$watch(attrs.todoFocus, function( newval ) {
       if ( newval ) {
