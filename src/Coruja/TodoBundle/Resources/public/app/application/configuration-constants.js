248d4bfafeac7f9a05413173bcd3a4b207964d4c
rommsen <sarom@web.de>
2012-11-13 12:54:15 +0100

    use base_url for constants

--- src/Coruja/TodoBundle/Resources/public/app/application/configuration-constants.js
+++ src/Coruja/TodoBundle/Resources/public/app/application/configuration-constants.js
@@ -6,10 +6,10 @@
  */
 
 Application.Constants.constant('configuration',   {
-        LOGIN_URL : 'http://plesynd/app_dev.php/login',
-        LOGOUT_URL : 'http://plesynd/app_dev.php/logout',
-        CONFIRM_URL : 'http://plesynd/app_dev.php/user/confirm/',
-        TODO_RESOURCE_URI : 'http://plesynd/app_dev.php/todo/api/todos/:todoId',
-        TODO_LIST_RESOURCE_URI : 'http://plesynd/app_dev.php/todo/api/lists/:todoListId'
+        LOGIN_URL : base_url+'/login',
+        LOGOUT_URL : base_url+'/logout',
+        CONFIRM_URL : base_url+'/user/confirm/',
+        TODO_RESOURCE_URI : base_url+'/todo/api/todos/:todoId',
+        TODO_LIST_RESOURCE_URI : base_url+'/todo/api/lists/:todoListId'
     }
 );
